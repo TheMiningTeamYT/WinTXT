64fd34ba4f07db67cc3fd24396c0704a6eeefc0b
TheMiningTeamYT <31364192+TheMiningTeamYT@users.noreply.github.com>
2020-09-07 00:11:52 -0500

    Add files via upload

--- edit.bat
+++ edit.bat
@@ -1,20 +1,22 @@
 @echo off
 cls
+set NormalName=Edit
+set lowercasename=edit
 color 17
+
 set arg1=%1
 set arg2=%2
 set arg3=%3
 (echo "%arg1%" | findstr /i /c:"-?" >nul ) && (goto :helparg1) || (echo. > nul)
 (echo "%arg2%" | findstr /i /c:"-?" >nul ) && (goto :helparg1) || (echo. > nul)
 (echo "%arg3%" | findstr /i /c:"-?" >nul ) && (goto :helparg1) || (echo. > nul)
-call wintext %arg1% %arg2% /n Edit
+call wintext %arg1% %arg2% /n %NormalName%
 color
 cls
 exit /b
 
 :helparg1
 cls
-set arg1=/h edit
 color
-call wintext "%arg1%"
+call wintext /h %lowercasename%
 exit /b
